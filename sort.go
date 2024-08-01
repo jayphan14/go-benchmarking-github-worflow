98f77d23c6fd063a93665d59481851a072edfb2d
Jay Phan <jphan@drw.com>
2024-08-01 09:34:47 -0400

    profile

--- sort.go
+++ sort.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
 	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
 )
 
 func Sort1(arr []int) []int {
@@ -46,4 +51,28 @@ func generateArray(size int) []int {
 
 func MySort(arr []int) []int {
 	return Sort1(arr)
+}
+
+func main() {
+	// Start HTTP server for pprof
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	// Create a file to write the CPU profile to
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	defer f.Close()
+
+	// Start CPU profiling
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	// Generate and sort the array
+	arr := generateArray(100000)
+	MySort(arr)
 }
